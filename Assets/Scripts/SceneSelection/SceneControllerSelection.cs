1500e80c2cca211896eb365a038711cf6d756417
loki <growingdever@gmail.com>
2015-05-16 00:21:46 +0900

    스페이스바를 누를 때마다 다음 스탯으로 넘어가면서 선택이 됨.

---
+++ Assets/Scripts/SceneSelection/SceneControllerSelection.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneControllerSelection : MonoBehaviour {
+
+	public UISprite[] SpriteSeeks;
+	public int SeekWidth;
+	public float SeekSpeed;
+
+	int _currAbility = 0;
+	IEnumerator _currSeekCoroutine;
+	bool _keyDownSpace;
+
+	void Start() {
+		StartCoroutine (ChooseNextAbility ());
+	}
+
+	void Update() {
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			_keyDownSpace = true;
+		}
+	}
+
+	IEnumerator ChooseNextAbility() {
+		while (_currAbility < SpriteSeeks.Length) {
+			_keyDownSpace = false;
+			_currSeekCoroutine = MoveSpriteSeek();
+			yield return StartCoroutine( _currSeekCoroutine );
+			_currAbility++;
+		}
+	}
+
+	IEnumerator MoveSpriteSeek() {
+		UISprite currSpriteSeek = SpriteSeeks [_currAbility];
+		while (!_keyDownSpace) {
+			currSpriteSeek.transform.localPosition += new Vector3(SeekSpeed, 0.0f, 0.0f);
+			if( currSpriteSeek.transform.localPosition.x >= (float)SeekWidth / 2 ) {
+				currSpriteSeek.transform.localPosition = new Vector3(-1.0f * SeekWidth / 2, 0.0f, 0.0f);
+			}
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+}
