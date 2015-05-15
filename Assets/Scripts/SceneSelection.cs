aaf9d3adf5a7adbf359d7a0db35f1bd04b930865
Garuya <keicoon15@gmail.com>
2015-05-16 02:38:08 +0900

    start scene

---
+++ Assets/Scripts/SceneSelection.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSelection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SceneStart/AnimationSprite.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSprite : MonoBehaviour {
+
+	private float currentFPS = 0f ;
+	public float FPS;
+	public string[] SpriteName;
+	private int _Index = 0;
+
+	void Start(){
+		Vector3 pos = transform.position;
+		pos.x = 2f;
+		transform.position = pos;
+	}
+	// Update is called once per frame
+	void Update () {
+		float speed = -0.5f * Time.deltaTime;
+
+		transform.position += new Vector3( speed, 0f, 0f );
+		currentFPS += Time.deltaTime;
+		if (currentFPS > (FPS/SpriteName.Length)) {
+			//Debug.Log("animation on");
+			if(_Index >= SpriteName.Length)
+				_Index = 0;
+			gameObject.GetComponent<UISprite>().spriteName = SpriteName[_Index++];
+			currentFPS = 0f;
+		}
+
+		//Debug.Log (transform.position.x);
+		if (transform.position.x < -2f) {
+			Vector3 pos = transform.position;
+			pos.x = 2f;
+			transform.position = pos;
+		}
+	}
+}
