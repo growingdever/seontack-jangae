46ff7a506b03b9b7cb6673a828be3eec3e4211dc
loki <growingdever@gmail.com>
2015-05-16 06:51:27 +0900

    배틀씬 다듬고 SpriteAnimation 만듬.

---
+++ Assets/Scripts/SpriteAnimation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteAnimation : MonoBehaviour {
+
+	public string[] SpriteNames;
+	public float Interval;
+
+	UISprite _sprite;
+	int _currIndex;
+	
+
+	void Start () {
+		_sprite = this.GetComponent<UISprite> ();
+		StartCoroutine (Animating ());
+	}
+
+	IEnumerator Animating() {
+		while (true) {
+			yield return new WaitForSeconds(Interval);
+			_currIndex++;
+			_currIndex = _currIndex % SpriteNames.Length;
+			_sprite.spriteName = SpriteNames[_currIndex];
+		}
+	}
+
+}
