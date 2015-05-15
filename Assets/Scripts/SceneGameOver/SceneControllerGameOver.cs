90c08a922f43ad5f3bc3d5aac522bc695150d536
loki <growingdever@gmail.com>
2015-05-16 05:11:26 +0900

    게임 오버 화면.

---
+++ Assets/Scripts/SceneGameOver/SceneControllerGameOver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneControllerGameOver : MonoBehaviour {
+
+	public UILabel LabelMessage;
+	public float TransitionTime;
+
+
+	public void OnFinishedFadeIn() {
+		StartCoroutine (Transition ());
+	}
+
+	IEnumerator Transition() {
+		yield return new WaitForSeconds (TransitionTime + 2);
+		TweenAlpha.Begin (LabelMessage.gameObject, TransitionTime, 0.0f).PlayForward();
+		yield return new WaitForSeconds (TransitionTime + 2);
+		Application.LoadLevel (0);
+	}
+	
+}
