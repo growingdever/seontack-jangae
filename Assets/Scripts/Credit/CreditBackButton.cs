ed4bbee759a1b560eac3b418a7f846132dd4c6cf
loki <growingdever@gmail.com>
2015-05-16 16:31:41 +0900

    크레딧!

---
+++ Assets/Scripts/Credit/CreditBackButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditBackButton : MonoBehaviour {
+
+	public void OnClick() {
+		Application.LoadLevel (0);
+	}
+}
---
+++ Assets/Scripts/SceneStart/ButtonCredit.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonCredit : MonoBehaviour {
+
+	void OnClick() {
+		Application.LoadLevel (4);
+	}
+}
