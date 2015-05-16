93cd8d4d45eaf46ea38257140f8b3653a540055a
loki <growingdever@gmail.com>
2015-05-16 11:56:29 +0900

    전투씬에서 장식물들 추가함.

---
+++ Assets/Scripts/FloatingSprite2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingSprite2 : MonoBehaviour {
+
+	public float Speed;
+	public float MaxHeight;
+	public float MinHeight;
+
+	void Update() {
+		this.transform.localPosition += new Vector3(Speed * Time.deltaTime, 0, 0);
+		Vector3 curr = this.transform.localPosition;
+		if (Speed > 0) {
+			if (curr.x > 640) {
+				curr.x = -640;
+				curr.y = Random.Range(MinHeight, MaxHeight);
+				this.transform.localPosition = curr;
+			}
+		} else {
+			if (curr.x < -640) {
+				curr.x = 640;
+				curr.y = Random.Range(MinHeight, MaxHeight);
+				this.transform.localPosition = curr;
+			}
+		}
+	}
+
+}
